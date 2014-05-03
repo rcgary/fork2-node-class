76c04386e23fd08465a07d0de47db587cd3e61dd
Chao Ruan <chaoruan818@gmail.com>
2014-05-03 21:08:50 +1000

    Implement Methods Inheritance

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-module.exports = function(args) {
+module.exports = function(args,parent) {
   var constructor = args.initialize || function () {};
+if (parent) {
+  constructor.prototype = new parent();
+}
+constructor.__super__ = parent || Object;
+constructor.prototype.constructor = constructor;
+
   for(var i in args) {
-    if (args[i].hasOwnProperty && typeof args[i] == 'function' && i != 'initialize') {
+    if (args[i].hasOwnProperty && typeof args[i] == 'function' &&
+        i != 'initialize') {
       constructor.prototype[i] = args[i];
     }
   }
