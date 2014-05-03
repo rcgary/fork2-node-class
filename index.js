c36e58f4ff1e1650dca07bd38fcf731b77ac06fc
Chao Ruan <chaoruan818@gmail.com>
2014-05-03 21:42:35 +1000

    Finished

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = function(args,parent) {
   var constructor = args.initialize || function () {};
-if (parent) {
-  constructor.prototype = new parent();
-}
-constructor.__super__ = parent || Object;
-constructor.prototype.constructor = constructor;
+  if (parent) {
+    constructor.prototype = new parent();
+  }
+  constructor.__super__ = parent || Object;
+  constructor.prototype.constructor = constructor;
 
   for(var i in args) {
     if (args[i].hasOwnProperty && typeof args[i] == 'function' &&
@@ -12,5 +12,15 @@ constructor.prototype.constructor = constructor;
       constructor.prototype[i] = args[i];
     }
   }
+
+  var current = constructor
+  constructor.prototype.super = function(method) {
+    current = current.__super__;
+    var result = current.prototype[method].apply(this, [].slice.call(arguments, 1));
+    // set current back
+    current = constructor;
+    return result;
+  };
+
   return constructor;
 };
